600698877cacaad188c7257b5f1f912e5c8acd7c
lutfan04 <lutfan04@gmail.com>
2021-10-01 14:07:30 +0700

    solusi jurnal 02

---
+++ Jurnal02.java
@@ -0,0 +1,7 @@
+public class Jurnal02 {
+	public static void main (String[] args) {
+		System.out.println("NIM  : 6706210099");
+		System.out.println("NAMA : LutfanDavi");
+		System.out.println("KELAS: D3RPLA-45-04");
+	}
+}
